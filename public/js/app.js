4993959d2133412ecdc7a4c2397cf6a02eece90f
umais <umaisahmed42@yahoo.com>
2020-04-03 06:12:34 -0700

    setup for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -16,7 +16,7 @@ if(!location){
 msg1.textContent="Loading...."
 
 
-fetch('http://localhost:3000/weather?address='+location).then((response)=>{
+fetch('/weather?address='+location).then((response)=>{
   response.json().then((data)=>{
     if(!data){
       msg1.textContent=data.error
